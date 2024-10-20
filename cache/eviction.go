e4b5b7dfa8a7cde61f6d77084a8d4b9b52c00f5b
Kaycee Nuel <heykayceee@gmail.com>
2024-10-21 00:08:07 +0100

    implementing evict policy

---
+++ cache/eviction.go
@@ -0,0 +1,27 @@
+package cache
+
+import (
+	"container/list"
+	"sync"
+)
+
+// Evictionpolicy is an interface that defines the methods for an eviction policy
+type Evictionpolicy interface {
+	RecordAceess(key string) //record the access of the key
+	RemoveEviction() string  // return the key to evict
+}
+
+// LRU is a struct that implements the Evictionpolicy interface
+type LRUCache struct {
+	Queue *list.List               // queue to store the keys in the order of their access
+	items map[string]*list.Element // map to store the keys and their positions in the queue
+	mu    sync.Mutex
+}
+
+// NewLRUCache creates a new LRUCache
+func NewLRUCache() *LRUCache {
+	return &LRUCache{
+		Queue: list.New(),
+		items: make(map[string]*list.Element),
+	}
+}
